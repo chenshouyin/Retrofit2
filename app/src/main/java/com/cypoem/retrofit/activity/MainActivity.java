7bedbe4b334cfccd2e3142197e221751c50f0d72
zhpan <zhangp@oitsme.com>
2018-03-22 11:11:11 +0800

    添加取消下载功能

--- app/src/main/java/com/cypoem/retrofit/activity/MainActivity.java
+++ app/src/main/java/com/cypoem/retrofit/activity/MainActivity.java
@@ -14,12 +14,15 @@ import android.widget.TextView;
 import com.cypoem.retrofit.R;
 import com.cypoem.retrofit.module.BasicResponse;
 import com.cypoem.retrofit.module.response.MeiZi;
+import com.cypoem.retrofit.net.Constants;
 import com.cypoem.retrofit.net.DefaultObserver;
-import com.cypoem.retrofit.net.IdeaApi;
 import com.cypoem.retrofit.net.download.DownloadListener;
+import com.cypoem.retrofit.net.IdeaApi;
+import com.cypoem.retrofit.net.RequestHelper;
 import com.cypoem.retrofit.net.download.DownloadUtils;
 import com.cypoem.retrofit.utils.LogUtils;
 import com.cypoem.retrofit.utils.ToastUtils;
+
 
 import java.io.BufferedInputStream;
 import java.io.File;
@@ -33,12 +36,11 @@ import io.reactivex.schedulers.Schedulers;
 import okhttp3.ResponseBody;
 
 public class MainActivity extends BaseActivity {
-    private Button btn;
     ProgressBar progressBar;
     TextView mTvPercent;
-    private static final String url = "http://www.oitsme.com/download/oitsme.apk";
+    private Button btn;
+    private static final String url="http://www.oitsme.com/download/oitsme.apk";
     private DownloadUtils downloadUtils;
-
     @Override
     protected int getLayoutId() {
         return R.layout.activity_main;
@@ -46,16 +48,16 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void init(Bundle savedInstanceState) {
-        initView();
-    }
-
-    private void initView() {
-        progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        mTvPercent = (TextView) findViewById(R.id.tv_percent);
+        progressBar= (ProgressBar) findViewById(R.id.progressBar);
+        mTvPercent=(TextView)findViewById(R.id.tv_percent);
         btn = (Button) findViewById(R.id.btn_download);
         downloadUtils = new DownloadUtils();
     }
 
+    //  登录 成功后保存token
+    public void login(View view) {
+        new RequestHelper(this).login();
+    }
 
     public void getData(View view) {
         IdeaApi.getApiService()
@@ -63,25 +65,27 @@ public class MainActivity extends BaseActivity {
                 .compose(this.<BasicResponse<List<MeiZi>>>bindToLifecycle())
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new DefaultObserver<BasicResponse<List<MeiZi>>>(this) {
+                .subscribe(new DefaultObserver<BasicResponse<List<MeiZi>>>(this, "正在加载...") {
                     @Override
                     public void onSuccess(BasicResponse<List<MeiZi>> response) {
                         List<MeiZi> results = response.getResults();
                         showToast("请求成功，妹子个数为" + results.size());
                     }
-                });
-    }
 
-    public void cancelDownload(View view) {
-        if (downloadUtils != null) {
-            downloadUtils.cancelDownload();
-            btn.setClickable(true);
-        }
+                    /**
+                     * token刷新成功后重新请求数据
+                     */
+                    @Override
+                    public void onTokenUpdateSuccess() {
+                        super.onTokenUpdateSuccess();
+                        getData(null);
+                    }
+                });
     }
 
     public void download(View view) {
         btn.setClickable(false);
-        downloadUtils.download(url, new DownloadListener() {
+        downloadUtils.download(Constants.DOWNLOAD_URL, new DownloadListener() {
             @Override
             public void onProgress(int progress) {
                 LogUtils.e("--------下载进度：" + progress);
@@ -134,11 +138,18 @@ public class MainActivity extends BaseActivity {
         }
     }
 
+    public void cancelDownload(View view) {
+        if (downloadUtils != null) {
+            downloadUtils.cancelDownload();
+            btn.setClickable(true);
+        }
+    }
+
     private void installApk(File file) {
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setDataAndType(Uri.fromFile(file),
-                "application/vnd.android.package-archive");
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setDataAndType(Uri.fromFile(file),
+                    "application/vnd.android.package-archive");
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
     }
 }
